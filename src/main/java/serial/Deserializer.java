bf450ed0954ac1b74e214455d3dcde8ccc4119f4
mrtea9 <ionmeriacri111@gmail.com>
2024-11-28 11:18:42 +0200

    ping

--- src/main/java/serial/Deserializer.java
+++ src/main/java/serial/Deserializer.java
@@ -1,12 +1,12 @@
 package serial;
 
 import lombok.RequiredArgsConstructor;
-import type.RString;
-import type.RValue;
+import type.*;
 import util.TrackedInputStream;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 @RequiredArgsConstructor
 public class Deserializer {
@@ -25,7 +25,16 @@ public class Deserializer {
         final int first = inputStream.read();
         if (first == -1) return null;
 
-        return parseString();
+        return switch (first) {
+            case Protocol.ARRAY -> parseArray();
+            case Protocol.SIMPLE_STRING -> parseString();
+            case Protocol.SIMPLE_ERROR -> new RError(parseString());
+            case Protocol.BULK_STRING -> likelyBlob ? parseBulkBlob() : parseBulkString();
+
+            default -> {
+                throw new IllegalArgumentException("Unexpected value: %s (%s)".formatted(first, (char) first));
+            }
+        };
     }
 
     private RString parseString() throws IOException {
@@ -42,5 +51,67 @@ public class Deserializer {
         }
 
         return RString.simple(builder.toString());
+    }
+
+    private RString parseBulkString() throws IOException {
+        final var length = parseLength();
+        final var bytes = inputStream.readNBytes(length);
+
+        inputStream.read();
+        inputStream.read();
+
+        return RString.bulk(new String(bytes));
+    }
+
+    private RBlob parseBulkBlob() throws IOException {
+        final var length = parseLength();
+        final var bytes = inputStream.readNBytes(length);
+
+        return RBlob.bulk(bytes);
+    }
+
+    private RValue parseArray() throws IOException {
+        final var length = parseLength();
+
+        if (length == -1) return RNil.SIMPLE;
+
+        if (length == 0) return RArray.empty();
+
+        final var array = new ArrayList<RValue>();
+        for (int index = 0; index < length; index++) {
+            array.add(read());
+        }
+
+        return RArray.view(array);
+    }
+
+    private int parseLength() throws IOException {
+        final var line = parseUntilEndOfLine();
+
+        if ("-1".equals(line)) return -1;
+
+        return Integer.parseUnsignedInt(line);
+    }
+
+    private String parseUntilEndOfLine() throws IOException {
+        final var builder = new StringBuilder();
+
+        var cariageReturn = false;
+
+        int value;
+        while ((value = inputStream.read()) != -1) {
+            if ('\n' == value && cariageReturn) {
+                break;
+            } else if ('\r' == value) {
+                cariageReturn = true;
+            } else {
+                if (cariageReturn) builder.append('\r');
+
+                builder.append((char) value);
+                cariageReturn = false;
+            }
+        }
+
+        return builder.toString();
     }
 }
--- src/main/java/serial/Serializer.java
+++ src/main/java/serial/Serializer.java
@@ -2,19 +2,20 @@ package serial;
 
 
 import lombok.RequiredArgsConstructor;
-import type.RString;
-import type.RValue;
+import type.*;
 import util.TrackedOutputStream;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class Serializer {
 
     private static final byte[] CRLF_BYTES = Protocol.CRLF.getBytes();
     private static final byte[] OK_BYTES = "OK".getBytes(StandardCharsets.US_ASCII);
+    private static final byte[] MINUS_ONE_BYTES = { '-', '1' };
 
     private final OutputStream outputStream;
 
@@ -28,11 +29,32 @@ public class Serializer {
 
     public void write(RValue value) throws IOException {
         switch (value) {
-            case RString string -> {
-                writeSimpleString(string.content());
+            case RArray<?> array -> writeArray(array.items());
+            case RBlob blob -> {
+                if (!blob.bulk()) throw new UnsupportedOperationException("non bulk blob are not supported");
+
+                writeBulkBytes(blob.content());
             }
-            default -> {
-                break;
+            case RError error -> writeError(error);
+            case RInteger integer -> writeSimpleInteger(integer.value());
+            case RNil nil -> {
+                if (nil.bulk()) {
+                    writeNilBulk();
+                } else {
+                    writeNil();
+                }
+            }
+            case ROk ok -> {
+                switch (ok) {
+                    case OK -> writeOk();
+                }
+            }
+            case RString string -> {
+                if (string.bulk()) {
+                    writeBulkString(string.content());
+                } else {
+                    writeSimpleString(string.content());
+                }
             }
         }
     }
@@ -43,4 +65,58 @@ public class Serializer {
         outputStream.write(CRLF_BYTES);
     }
 
+    private void writeSimpleInteger(Integer integer) throws IOException {
+        outputStream.write(Protocol.INTEGER);
+        outputStream.write(String.valueOf(integer).getBytes());
+        outputStream.write(CRLF_BYTES);
+    }
+
+    private void writeBulkString(String string) throws IOException {
+        writeBulkBytes(string.getBytes());
+        outputStream.write(CRLF_BYTES);
+    }
+
+    private void writeBulkBytes(byte[] bytes) throws IOException {
+        outputStream.write(Protocol.BULK_STRING);
+
+        outputStream.write(String.valueOf(bytes.length).getBytes());
+        outputStream.write(CRLF_BYTES);
+
+        outputStream.write(bytes);
+    }
+
+    private void writeNilBulk() throws IOException {
+        outputStream.write(Protocol.BULK_STRING);
+        outputStream.write(MINUS_ONE_BYTES);
+        outputStream.write(CRLF_BYTES);
+    }
+
+    private void writeArray(List<? extends RValue> list) throws IOException {
+        outputStream.write(Protocol.ARRAY);
+        outputStream.write(String.valueOf(list.size()).getBytes());
+        outputStream.write(CRLF_BYTES);
+
+        for (final var element : list) {
+            write(element);
+        }
+    }
+
+    private void writeError(RError error) throws IOException {
+        final var message = error.message().content();
+
+        outputStream.write(Protocol.SIMPLE_ERROR);
+        outputStream.write(message.getBytes());
+        outputStream.write(CRLF_BYTES);
+    }
+
+    private void writeOk() throws IOException {
+        outputStream.write(Protocol.SIMPLE_STRING);
+        outputStream.write(OK_BYTES);
+        outputStream.write(CRLF_BYTES);
+    }
+
+    private void writeNil() throws IOException {
+        outputStream.write(Protocol.NULL);
+        outputStream.write(CRLF_BYTES);
+    }
 }
